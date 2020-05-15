424e0423be39046c826e26e0aca4b901ef00a0e3
BuildTools <gbluntzer@gmail.com>
2020-05-14 23:31:37 -0500

    Added Last Score to Try again screen

--- scenes/GameOver.gd
+++ scenes/GameOver.gd
@@ -1,6 +1,8 @@
 extends Control
 
-
+func _ready():
+	var gameState = get_node("/root/GameState")
+	$CenterContainer/VBoxContainer/LastScore.text= str(gameState.score)
 
 func _on_TryAgainButton_pressed():
 	get_tree().call_group("GameState","reset")
